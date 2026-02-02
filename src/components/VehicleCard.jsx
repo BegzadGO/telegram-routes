ba13fb75eb37ccc12734970d05e77346cb288137
BegzadGO <ilhamkarilich27@gmail.com>
2026-02-02 22:36:17 +0500

    Update VehicleCard.jsx

--- src/components/VehicleCard.jsx
+++ src/components/VehicleCard.jsx
@@ -2,6 +2,11 @@ import React from 'react';
 
 const VehicleCard = ({ vehicle }) => {
   const { vehicle_name, driver_name, driver_phone } = vehicle;
+  const formatDriverName = (name) => {
+    if (!name) return '';
+    const firstName = name.trim().split(' ')[0];
+    return firstName.charAt(0).toUpperCase() + firstName.slice(1).toLowerCase();
+  };
 
   return (
     <div className="vehicle-card">
@@ -9,7 +14,9 @@ const VehicleCard = ({ vehicle }) => {
       {/* Верхняя строка */}
       <div className="vehicle-card-top">
         <div className="vehicle-name">{vehicle_name}</div>
-        <div className="driver-name">{driver_name}</div>
+        <div className="driver-name">
+  {formatDriverName(driver_name)}
+</div>
       </div>
 
       {driver_phone && (
