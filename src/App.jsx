1a14a74841f6a1cbc215a40ef5df34eacda4e802
BegzadGO <ilhamkarilich27@gmail.com>
2026-02-04 16:34:56 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import RouteSelector from './components/RouteSelector';
 import VehicleList from './components/VehicleList';
 import { fetchRoutes, fetchVehiclesByRoute, fetchRoutePlaces } from './supabase';
